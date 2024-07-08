3b66a1aa914ad3c49e20924472b234edadae6882
Kevin Rangel <code@venikx.com>
2024-07-08 17:06:29 +0200

    fix: typo in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   # Inspiration: https://github.com/akirak/flake-templates
   description = "A collection of nix flake project templates";
 
-  outputs = {...}: {
+  outputs = { ... }: {
     templates = {
       meta = {
         path = ./python-flake;
-        description = "Flake for Flask project, includes formatting and linting.";
+        description =
+          "Flake for Flask project, includes formatting and linting.";
       };
     };
   };
-}}
+}
