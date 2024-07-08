a930aca3bee38fbbb98a26c7a0a654cb424e169d
Kevin Rangel <code@venikx.com>
2024-07-08 17:00:44 +0200

    docs: add readme

--- python-flask/flake.nix
+++ python-flask/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Application packaged using poetry2nix";
+  description = "Python application packaged using poetry2nix";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
