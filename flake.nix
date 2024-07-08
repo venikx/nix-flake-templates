0e7ef09faae800760c3f079b0e3265684cd0f823
Kevin Rangel <code@venikx.com>
2024-07-08 16:52:40 +0200

    feat: add python flask project template

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  # Inspiration: https://github.com/akirak/flake-templates
+  description = "A collection of nix flake project templates";
+
+  outputs = {...}: {
+    templates = {
+      meta = {
+        path = ./python-flake;
+        description = "Flake for Flask project, includes formatting and linting.";
+      };
+    };
+  };
+}}
